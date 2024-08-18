9794f8cbbc1979e6f7d5faf874423bc546167fb6
rad5001 <>
2024-08-19 00:39:19 +0200

    fix 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,10 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux"
     }
-    result = requests.get(url, headers=headers, allow_redirects=False)
-    if result.status_code == 404:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    return result.json().get("data").get("subscribers")
+    results = response.json().get("data")
+    return results.get("subscribers")
